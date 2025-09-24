46c8991dbf2020847da478395180be520bea5cec
FSHimel <shahriarfahim067@gmail.com>
2025-09-24 17:22:15 +0600

    first commit

---
+++ src/Components/Countries.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Countries extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Countries I have visited</h1>
+            </div>
+        );
+    }
+}
+
+export default Countries;
